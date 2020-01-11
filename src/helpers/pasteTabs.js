e0a84af7323df96615fc6748955d9bd1a152ea07
Eitan <eitan1112@gmail.com>
2020-01-11 14:44:46 +0200

    first try

--- src/helpers/pasteTabs.js
+++ src/helpers/pasteTabs.js
@@ -1,18 +1,19 @@
-import parsePaste from '../libs/parsePaste'
+import { tabsRowPattern, sectionPattern } from '../libs/pasteRegexPattern'
 
-export default (state, rowIndex, tabs, pasteType) => {
-  const matches = parsePaste(tabs)
+const oldPaste = (state, rowIndex, tabs, pasteType) => {
+  console.log(tabs.split('\n'))
+  const matches = tabsRowPattern(tabs)
   if (matches.length % 6 !== 0 || matches.length === 0) {
     // If the pattern didn't match multiplie of 6 rows
-    return state;
+    return state
   }
-  let newState = [];  
+  let newState = []
   for (let i = 0; i < matches.length; i = i + 6) {
-    let newRow = { lines: [], section: undefined, lyrics: undefined };
+    let newRow = { lines: [], section: undefined, lyrics: undefined }
     for (let j = i; j < i + 6; j++) {
-      newRow.lines.push(matches[j][2]);
+      newRow.lines.push(matches[j][2])
     }
-    newState.push(newRow);
+    newState.push(newRow)
   }
   switch (pasteType) {
     case 'REPLACE':
@@ -24,4 +25,45 @@ export default (state, rowIndex, tabs, pasteType) => {
     default:
       return state
   }
-};
+}
+
+const newPaste = (state, rowIndex, paste, pasteType) => {
+  paste = paste.split('\n')
+  let newState = []
+
+  let tempRow = {
+    lines: [],
+    section: undefined,
+    lyrics: undefined
+  }
+
+  paste.forEach((line) => {
+    const sectionMatches = sectionPattern(line)
+    const rowsMatches = tabsRowPattern(line)
+    if (sectionMatches != null && sectionMatches.length === 1) {
+      tempRow.section = sectionMatches[0]
+    }
+    if (rowsMatches !== null && rowsMatches.length === 1) {
+      tempRow.lines.push(rowsMatches[0][2])
+    } else if (tempRow.lines.length !== 0) {
+      tempRow = {
+        lines: [],
+        section: undefined,
+        lyrics: undefined
+      }
+    }
+    if (tempRow.lines.length === 6) {
+      newState.push(tempRow)
+      tempRow = {
+        lines: [],
+        section: undefined,
+        lyrics: undefined
+      }
+    }
+
+  })
+
+  return newState
+}
+
+export default newPaste
