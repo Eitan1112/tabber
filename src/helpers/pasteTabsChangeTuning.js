1735b19fc8b69ec2552084d28b0077b6edd3a384
Eitan <eitan1112@gmail.com>
2020-01-11 14:48:37 +0200

    Fixed regex-responsive conflict

--- src/helpers/pasteTabsChangeTuning.js
+++ src/helpers/pasteTabsChangeTuning.js
@@ -1,13 +1,13 @@
-import parsePaste from '../libs/parsePaste'
+import {tabsRowPattern} from '../libs/pasteRegexPattern'
 
 export default (state, tabs) => {
-    const matches = parsePaste(tabs)
+    const matches = tabsRowPattern(tabs)
 
     if (matches.length % 6 !== 0 || matches.length === 0) {
         return state
     }
     let tuning = []
-    for (let i = 0; i < 6; i++) {        
+    for (let i = 0 ; i < 6 ; i++) {        
         tuning.push(matches[i][1].trim())
     }
     return {
