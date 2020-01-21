5bdaec5644ca220e3ce8d41a414ea246517bcaa4
Eitan <eitan1112@gmail.com>
2020-01-21 21:33:57 +0200

    tutorial almost finished

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,163 +1,113 @@
 import React from 'react'
 import { Modal, Icon } from 'antd'
-import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
-import getPageInfo, {pages} from '../libs/getTutorialPage'
+import getPageInfo, { pages } from '../libs/getTutorialPage'
+import { reset } from '../actions/tabwriter'
 
-function getElementPropertyValue( element, property ) {
-    return window.getComputedStyle(element, null).getPropertyValue(property)
-}
-
-const changeLocation = (elem, x, y) => {
-    const left = parseInt(getElementPropertyValue(elem, 'left'), 10)
-    const top = parseInt(getElementPropertyValue(elem, 'top'), 10)
-    const dx = left - x
-    const dy = top - y
-    let i = 1
-    const count = 20
-    const delay = 20
-    
-    const loop = () => {
-        if ( i >= count ) { 
-            return
-        }
-        i += 1
-        elem.style.left = (left - (dx * i / count) ).toFixed(0) + 'px'
-        elem.style.top = (top - (dy * i / count) ).toFixed(0) + 'px'
-        setTimeout(loop, delay)
-    }    
-    loop();
-}
-
-const changeSize = (elem, x, y) => {
-    const width = parseInt(getElementPropertyValue(elem, 'width'), 10)
-    const height = parseInt(getElementPropertyValue(elem, 'height'), 10)
-    const dx = width - x
-    const dy = height - y
-    let i = 1
-    const count = 20
-    const delay = 20
-    
-    const loop = () => {
-        if ( i >= count ) { 
-            return
-        }
-        i += 1
-        elem.style.width = (width - (dx * i / count) ).toFixed(0) + 'px'
-        elem.style.height = (height - (dy * i / count) ).toFixed(0) + 'px'
-        setTimeout(loop, delay)
-    }    
-    loop();
-}
-
-class Keyboard extends React.Component {
-    render() {
-        return (
-            <div className="keyboard-wrapper">
-                <div className="keyboard-image">
-                    <div className="keyboard-color">
-                        <div className="keyboard-highlight" id="keyboard-highlight-1"></div>
-                        <div className="keyboard-highlight" id="keyboard-highlight-2"></div>
-                    </div>
+const Keyboard = (props) => {
+    return (
+        <div className="keyboard-wrapper">
+            <div className="keyboard-image">
+                <div className="keyboard-color">
+                    <div className="keyboard-highlight" id="keyboard-highlight-1" style={props.styles[0]}></div>
+                    <div className="keyboard-highlight" id="keyboard-highlight-2" style={props.styles[1]}></div>
                 </div>
             </div>
-        )   
-    }
+        </div>
+    )
 }
 
 class Tutorial extends React.Component {
     constructor(props) {
-        super(props);    
-        this.state = { visible: false, page: 0, playing: false }
+        super(props);
+        this.state = {
+            visible: false,
+            page: undefined,
+            styles: undefined,
+            text: undefined
+        }
     }
 
-    showModal = () => {
-        this.setState({
-            visible: true
-        });
-    };
-
-    handleStartTutorial = () => {
-        ReactDOM.render(<Keyboard />, document.getElementsByClassName('ant-modal-mask')[0])
-        this.setState({ playing: true })
-        this.handleChangePage()
-    };
-
-    handleChangePage = (goBy = 1) => {
-        const page = typeof goBy === 'number' ? this.state.page + goBy : this.state.page + 1
-        if (page <= pages) {
-            const { left, top, width, height, text, dispatch } = getPageInfo(page)
-            this.setState({ page })
-            document.getElementById('tutorial-text').innerHTML = text
-            const element = document.getElementById('keyboard-highlight-1')
-            changeLocation(element, left, top)
-            changeSize(element, width, height)
-            dispatch.forEach((action, index) => {
-                setTimeout(() => this.props.dispatch(action()), index * 750)
+    toggleModal = () => {
+        if (this.state.visible) { // Closing
+            this.props.dispatch(reset())
+            this.setState({
+                visible: !this.state.visible,
+                page: undefined,
+                styles: undefined,
+                text: undefined
+            })
+        } else { // Opening
+            this.setState({
+                visible: !this.state.visible,
+                page: 0,
+                styles: getPageInfo(0).styles,
+                text: getPageInfo(0).text
             })
         }
     }
 
-    firstPageText = 'Do you want to watch the tutorial?'
+    handleNext = () => {
+        const isAtEnd = this.state.page === pages
+        if (isAtEnd) {
+            this.setState({ visible: false, page: undefined, styles: undefined })
+        } else {
+            const { styles, text, dispatch } = getPageInfo(this.state.page + 1)
+            dispatch.forEach((action, index) => setTimeout(() => this.props.dispatch(action()), (index + 1) * 150))
+            this.setState({
+                page: this.state.page + 1,
+                styles: styles,
+                text
+            })
+        }
+    }
 
-    handleCancel = e => {
-        document.getElementById('tutorial-text').innerHTML = this.firstPageText
-        this.setState({
-            visible: false,
-            page: 0,
-            playing: false
-        },)
-        document.getElementsByClassName('ant-modal-mask')[0].innerHTML = ''
-    };
-
+    handlePrev = () => {
+        const isAtStart = this.state.page === 0
+        if (isAtStart) {
+            this.setState({ visible: false, page: undefined, styles: undefined })
+        } else {
+            const { styles, text } = getPageInfo(this.state.page - 1)
+            this.setState({
+                page: this.state.page - 1,
+                styles: styles,
+                text
+            })
+        }
+    }
 
     render() {
         return (
             <span>
-                <span onClick={this.showModal}>
+                {
+                    this.state.page < pages && <Keyboard styles={this.state.styles} />
+                }
+                <span onClick={this.toggleModal}>
                     {this.props.children}
                 </span>
                 <Modal
                     title="Tutorial"
                     visible={this.state.visible}
-                    onCancel={this.handleCancel}
+                    onCancel={this.toggleModal}
                     footer={(
                         <div className="tutorial-container">
-                        <button 
-                        className="tutorial-button tutorial-button-no" 
-                        onClick={this.state.playing ? () => this.handleChangePage(-1) : this.handleCancel}>
-                        {
-                            this.state.playing ?                                
-                            <Icon type="caret-left" />
-                            : 
-                            'No'
-                        }
-                        </button>
-                        <button 
-                        className="tutorial-button tutorial-button-yes" 
-                        onClick={
-                            this.state.playing ?
-                                this.state.page === pages ?
-                                this.handleCancel
-                                :
-                                this.handleChangePage 
-                            : 
-                            this.handleStartTutorial
-                        }>
-                        {
-                            this.state.playing ?
-                            <Icon type="caret-right" />
-                            : 
-                            'Yes'
-                        }
-                        </button>
+                            <button
+                                className="tutorial-button tutorial-button-no"
+                                onClick={this.handlePrev} >
+                                <Icon type="caret-left" />
+                            </button>
+                            <button
+                                className="tutorial-button tutorial-button-yes"
+                                onClick={this.handleNext}>
+                                <Icon type="caret-right" />
+                            </button>
                         </div>
                     )}
                     width="300px">
                     <div>
                         <span id="tutorial-text">
-                            {this.firstPageText}
-                        </span>                           
+                            {this.state.text}
+                        </span>
                     </div>
                 </Modal>
             </span>
@@ -166,6 +116,6 @@ class Tutorial extends React.Component {
 }
 
 
-const mapStateToProps = (state) => ({dispatch: state.dispatch})
+const mapStateToProps = (state) => ({ dispatch: state.dispatch })
 
 export default connect(mapStateToProps)(Tutorial)
--- src/libs/getTutorialPage.js
+++ src/libs/getTutorialPage.js
@@ -1,64 +1,144 @@
-import { goRight } from '../actions/coords' 
-import { musicBreak, replaceChar, backspace, downline } from '../actions/tabwriter'
+import React from 'react'
+import { goRight, goLeft, goRowStart, goRowEnd, goStart, goEnd } from '../actions/coords'
+import { musicBreak, replaceChar, backspace, downline, middleSpace, reset } from '../actions/tabwriter'
+
+const backgroundColor = `rgba(${150}, ${150}, ${150}, ${0.5})`
 
 export default (page) => {
-        switch (page) {
-            case 1:
-                return {
+    switch (page) {
+        case 0:
+            return {
+                styles: [{
                     left: 535,
                     top: 170,
                     width: 110,
                     height: 80,
-                    text: 'Hit the arrows to move around',
-                    dispatch: [goRight, goRight]
-                }
-            case 2:
-                return {
+                }, { display: 'none' }],
+                text: 'Hit the arrows to move around',
+                dispatch: [goRight, goRight]
+            }
+        case 1:
+            return {
+                styles: [{
                     left: 50,
                     top: 70,
                     width: 344,
                     height: 40,
-                    text: 'Hit any number to insert it',
-                    dispatch: [replaceChar, goRight]
-                }
-            case 3:
-                return {
+                }, { display: 'none' }],
+                text: 'Hit any number to insert it',
+                dispatch: [replaceChar, goRight]
+            }
+        case 2:
+            return {
+                styles: [{
                     left: 142,
                     top: 205,
                     width: 220,
                     height: 40,
-                    text: 'Hit space to add a music break',
-                    dispatch: [musicBreak]
-                }
-            case 4:
-                return {
+                }, { display: 'none' }],
+                text: 'Hit Space to add a music break',
+                dispatch: [musicBreak]
+            }
+        case 3:
+            return {
+                styles: [{
                     left: 460,
                     top: 70,
                     width: 70,
                     height: 40,
-                    text: 'Hit backspace to delete',
-                    dispatch: [backspace]
-                }
-            case 5:
-                return {
+                }, { display: 'none' }],
+                text: 'Hit Bbackspace to delete',
+                dispatch: [backspace]
+            }
+        case 4:
+            return {
+                styles: [{
                     left: 475,
                     top: 100,
                     width: 60,
                     height: 80,
-                    text: 'Hit enter to add another line',
-                    dispatch: [downline]
+                }, { display: 'none' }],
+                text: 'Hit Enter to add another line.',
+                dispatch: [downline]
+            }
+        case 5:
+            return {
+                styles: [{
+                    left: 142,
+                    top: 205,
+                    width: 220,
+                    height: 40,
+                }, {
+                    left: 15,
+                    top: 170,
+                    width: 85,
+                    height: 40,
                 }
-            default:
-                return {
+                ],
+                text: 'Hit Shift + Space to add space between chars.',
+                dispatch: [goRight, replaceChar, goRight, replaceChar, goLeft, middleSpace]
+            }
+        case 6:
+            return {
+                styles: [{
+                    left: 535,
+                    top: 40,
+                    width: 110,
+                    height: 80,
+                }, { display: 'none' }],
+                text: 'Hit End, Home, PgUp, PgDn to move to start / end of row / song',
+                dispatch: [goRowStart, goRowEnd, goStart, goEnd]
+            }
+        case 7:
+            return {
+                styles: [{
+                    left: 80,
+                    top: 100,
+                    width: 400,
+                    height: 100,
+                }, { display: 'none' }],
+                text: 'Hit special chars to add slides, pull-off, hammer etc.',
+                dispatch: [goRight, replaceChar, goRight, () => replaceChar('h'), goRight, () => replaceChar(3)]
+            }
+        case 8:
+            return {
+                styles: [{
+                    position: 'fixed',
+                    backgroundColor,
+                    left: 75,
+                    top: 45,
+                    width: 60,
+                    height: 55,
+                }, { display: 'none' }],
+                text: 'Play your tabs.',
+                dispatch: []
+            }
+        case 9:
+            return {
+                styles: [{
+                    position: 'fixed',
+                    backgroundColor: backgroundColor,
+                    left: 200,
+                    top: 45,
+                    width: 60,
+                    height: 55,
+                }, { display: 'none' }],
+                text: 'Paste tabs from anywhere!',
+                dispatch: []
+            }
+        default:
+            return {
+                styles: [{
                     left: 0,
                     top: 0,
                     width: 0,
                     height: 0,
-                    text: 'You finished the tutorial!',
-                    dispatch: []
-                }
+                }, { display: 'none' }],
+                text: 'You finished the tutorial!',
+                dispatch: [reset]
+            }
 
-        }
     }
+}
 
-export const pages = 6
+export const pages = 10
