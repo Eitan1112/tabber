cf392dfea08ec6960d941610c393d2595b9b3ffd
Eitan <eitan1112@gmail.com>
2020-02-13 19:05:26 +0200

    fixed char

--- src/helpers/char.js
+++ src/helpers/char.js
@@ -1,12 +1,8 @@
-// import mapStateLine from "../libs/mapStateLine"
-import mapStateRow from "../libs/mapStateRow"
-
+import mapStateLine from "../libs/mapStateLine"
 
 export default (state, action, { rowIndex, lineIndex, charIndex }) => {  
-    const rowLength = state[rowIndex].lines[0].length
-    const append = charIndex < rowLength - 2 ? '' : charIndex === rowLength - 2 ? '-' : '--'
-    return mapStateRow(state, rowIndex, (content) => (
-            content.slice(0, charIndex) + action.char + content.slice(charIndex + 1, content.length).concat(append)
+    return mapStateLine(state, rowIndex, lineIndex, (content) => (
+            content.slice(0, charIndex) + action.char + content.slice(charIndex + 1, content.length)
         )
     )
 }
